4774604f2fe53f847a784553c726b04a5be09ee7
Igal <igalbog@gmail.com>
2022-03-31 16:15:04 +0300

    not working yet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import "./App.css";
 
 function App() {
+  const formEl = useRef();
+  const [result, setResult] = useState();
+
+  const url = "https://localhost:5000";
+
+  const calculate = async (request) => {
+    const response = await fetch(`${url}/calc/`, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(request),
+    });
+
+    const data = await response.json();
+    console.log(data);
+    setResult(data);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(formEl.current.elements);
+
+    const reqData = {
+      num1: formEl.current.elements[0].value,
+      num2: formEl.current.elements[2].value,
+      oper: formEl.current.elements[1].value,
+    };
+
+    console.log(reqData);
+    calculate(reqData);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Calculator</h1>
+      <form ref={formEl} onSubmit={handleSubmit}>
+        <input type="text" id="first" />
+        <select id="operation" name="operation">
+          <option value="+">+</option>
+          <option value="-">-</option>
+          <option value="*">*</option>
+          <option value="/">/</option>
+        </select>
+        <input type="text" id="second" />
+        <input type="submit" value="Submit" />
+      </form>
+      <h1>{result}</h1>
     </div>
   );
 }
